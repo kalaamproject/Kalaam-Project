cbbe28c0958b238e0a4e5c49e4e2093583898492
andre wilkinson <andrefullstack@andres-MBP-2.fios-router.home>
2023-03-14 01:16:05 -0400

    navbar spaced

--- kalaam-project/src/layout/NavBar.js
+++ kalaam-project/src/layout/NavBar.js
@@ -1,25 +1,42 @@
 import React from "react";
+
 import "./NavBar.css"
 import { NavLink } from "react-router-dom";
+
+// const pages = ['News', 'About', 'Sponser', 'Contact', 'Donate'];
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 
 
 
 const NavBar = () => {
-    return(
-        <div className="navbar">
-            <h1><a href="/"> Kalaam</a></h1>
-            <ul>
-                <li><NavLink exact to="/news">News</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/sponser">Sponser</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-                <li><NavLink to="/donate">Donate</NavLink></li>
-            </ul>
 
+    return(
+<div className="navbar">
+        
+
+
+
+
+
+
+
+
+           
+
+
+                        <h1><a href="/"> Kalaam</a></h1>
+                
+                        <ul>
+                            <li><NavLink exact to="/news">News</NavLink></li>
+                            <li><NavLink to="/about">About</NavLink></li>
+                            <li><NavLink to="/sponser">Sponser</NavLink></li>
+                            <li><NavLink to="/contact">Contact</NavLink></li>
+                            <li><NavLink to="/donate">Donate</NavLink></li>
+                        </ul>
+
+                    
         </div>
     )
 }
-
-
 export default NavBar;
